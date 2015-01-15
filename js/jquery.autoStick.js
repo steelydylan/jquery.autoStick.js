a20ad369050ad787d3bc907e65acad63951bd392
steelydylan <ess_president@me.com>
2015-01-15 12:34:05 +0900

    bug fix

--- js/jquery.autoStick.js
+++ js/jquery.autoStick.js
@@ -5,58 +5,69 @@
 * @link http://horicdesign.com
 * @license MIT License
 */
+
 (function($){
-	var defaults = {
-		wrapperElement:"body",
-		beforeElement:null,
-		marginTop:20,
-		marginBottom:20,
-		enableCondition:function(){
-			return true;
-		}
-	}
-	$.fn.extend({
-		autoStick:function(opt){
-			opt = $.extend(defaults,opt);
-			$wrapper = $(opt.wrapperElement);
-			$wrapper.css("position","relative");
-			var $this = $(this);
-			var that = this;
-			$(window).resize(function(){
-				if(!opt.enableCondition.apply(that)){
-					$this.css("position","static");
-					return true;
-				}
-			});
-			$(window).scroll(function(){	
-				if(!opt.enableCondition.apply(that)){
-					$this.css("position","static");
-					return true;
-				}			
-				var scroll = $(this).scrollTop();
-				var height = 0;
-				var beforeElement = opt.beforeElement;
-				var beforeHeight = 0;
-				if(beforeElement){
-					beforeHeight = $(beforeElement).offset().top + $(beforeElement).height();
-				}
-				height = beforeHeight || $wrapper.offset().top;
-				if(scroll > height - opt.marginTop){
-					if(scroll + $this.height() + opt.marginBottom > $wrapper.offset().top + $wrapper.height()){
-						var bottom = $wrapper.offset().top + $wrapper.outerHeight() - $this.outerHeight();
-						$this.css("position","absolute");
-						$this.css("top",bottom - opt.marginBottom);
-					}else{
-						$this.css("position","fixed");
-						$this.css("top",opt.marginTop);
-					}
-				}else{
-					$this.css("position","absolute");
-					if(beforeHeight){
-						$this.css("top",beforeHeight);
-					}
-				}
-			});
-		}
-	})
+    var defaults = {
+        wrapperElement:"body",
+        beforeElement:null,
+        marginTop:20,
+        marginBottom:20,
+        marginLeft:10,
+        enableCondition:function(){
+            return true;
+        }
+    }
+    $.fn.extend({
+        autoStick:function(opt){
+            opt = $.extend(defaults,opt);
+            $wrapper = $(opt.wrapperElement);
+            $wrapper.css("position","relative");
+            var $this = $(this);
+            var that = this;
+            $(window).resize(function(){
+                if(!opt.enableCondition.apply(that)){
+                    $this.css("position","static");
+                    return true;
+                }
+            });
+            var width = $(this).width();
+            $(window).scroll(function(){   
+                if(!opt.enableCondition.apply(that)){
+                    $this.css("position","static");
+                    return true;
+                }          
+                var scroll = $(this).scrollTop();
+                var height = 0;
+                var beforeElement = opt.beforeElement;
+                var beforeHeight = 0;
+                if(beforeElement){
+                    beforeHeight = $(beforeElement).offset().top + $(beforeElement).height();
+                }
+                height = beforeHeight || $wrapper.offset().top;
+                if(scroll > height - opt.marginTop){
+                    if(scroll + $this.height() + opt.marginBottom > $wrapper.offset().top + $wrapper.height()){
+                        var bottom = $wrapper.outerHeight() - $this.outerHeight();
+                        $this.css("position","absolute");
+                        $this.css("top",bottom - opt.marginBottom);
+                        $this.css("left",opt.marginLeft);
+                        $this.width("auto");
+                        width = $this.width();
+                    }else{
+                        $this.css("position","fixed");
+                        $this.css("top",opt.marginTop);
+                        $this.css("left",$(beforeElement).offset().left);
+                        $this.width(width);
+                    }
+                }else{
+                    $this.css("position","absolute");
+                    if(beforeHeight){
+                        $this.css("top",beforeHeight - $(beforeElement).offset().top);
+                        $this.css("left",opt.marginLeft);
+                        $this.width("auto");
+                        width = $this.width();
+                    }
+                }
+            });
+        }
+    })
 })(jQuery);
